4f8a1fb33f34701c74cee96a31fcca789544a94e
eleonore-charvin <charvin.eleonore@gmail.com>
2025-02-24 18:46:47 +0100

    Update ThingspeakAdaptor

--- thingspeak_adaptor/Thingspeak_Adaptor.py
+++ thingspeak_adaptor/Thingspeak_Adaptor.py
@@ -6,18 +6,20 @@ import time
 import uuid
 
 class Thingspeak_Adaptor:
+
     def __init__(self, settings):
         """
         Initialize Thingspeak_Adaptor.
-        :param settings: Settings of hingspeak_Adaptor 
+        
+        Parameters:
+            settings (dict): Settings of Thingspeak_Adaptor.
         """
         # Load the settings
         self.settings = settings
         self.catalogURL = settings["catalogURL"]
         self.serviceInfo = settings['serviceInfo']
-        self.baseURL = self.settings["ThingspeakURL"]
-        self.channelWriteAPIkey = self.settings["ChannelWriteAPIkey"]
-        self.channelReadAPIkey = self.settings["ChannelReadAPIKey"]
+        self.baseURL = self.settings["thingspeakURL"]
+        self.userAPIKey = self.settings["userAPIKey"]
         self.broker = self.settings["brokerIP"]
         self.port = self.settings["brokerPort"]
         self.topic = self.settings["mqttTopic"] + "/#"
@@ -54,28 +56,36 @@ class Thingspeak_Adaptor:
     def notify(self, topic, payload):
         """
         Method called when a message is received.
-        :param topic: topic of the message
-        :param payload: payload of he message
+        
+        Parameters:
+            topic (str): topic of the message.
+            payload (json): payload of the message.
         """
         # Structure of the message:
-        # {'bn':f'SensorREST_MQTT_{self.deviceID}','e':[{'n':'humidity','v':'', 't':'','u':'%'}]}
+        # {'bn':f'group06/SmartGreenhouse/{greenhouseID}/{zoneID}','e':[{'n':'humidity','v':'', 't':'','u':'%'}]}
 
         # Decode the message
         message_decoded = json.loads(payload)
         message_value = message_decoded["e"][0]["v"]
         decide_measurement = message_decoded["e"][0]["n"]
+        greenhouseID = int(message_decoded["bn"].split("/")[-2])
+        zoneID = int(message_decoded["bn"].split("/")[-1])
         error = False
 
         # Match the measurement with the field
         # In Thingspeak website, we decided that:
         # - field 1 = temperature
-        # - field 2 = moisture
+        # - field 2 = moisture of zone 1
+        # - field 3 = moisture of zone 2
+        # ...
+        # - field 8 = moisture of zone 7 (if it exists)
         if decide_measurement == "temperature":
             print("\n \n Temperature Message")
             field_number = 1
         elif decide_measurement == "moisture":
-            print("\n \n Humidity Message")
-            field_number = 2
+            print("\n \n Moisture Message")
+            if (zoneID > 0) and (zoneID < 8):
+                field_number = zoneID + 1
         else: 
             error = True
 
@@ -83,24 +93,113 @@ class Thingspeak_Adaptor:
             print("Error")
         else:
             print(message_decoded)
+
+            # Get the write API key of the channel corresponding to the greenhouse
+            channel_write_api_key = self.getGreenhouseWriteAPIKey(greenhouseID)
+
             # Upload the value on Thingspeak
-            self.uploadThingspeak(field_number=field_number, field_value=message_value)
+            self.uploadThingspeak(channel_write_api_key=channel_write_api_key, field_number=field_number, field_value=message_value)
     
-    def uploadThingspeak(self, field_number, field_value):
+    def getGreenhouseWriteAPIKey(self, greenhouseID):
         """
-        Upload a value in a field on Thingspeak.
-        :param field_number: number of the field
-        :param field_value: value to upload
+        Get the write API key of the channel corresponding to the greenhouse.
+        If there is no channel associated with this greenhouse, a new one is created.
+
+        Parameters:
+            greenhouseID (int): ID of the greenhouse.
+
+        Returns:
+            str: write API key of the channel.
+        """
+        # Get the greenhouse form the catalog
+        greenhouse = requests.get(f"{self.catalogURL}/getGeenhouse?greenhouseID={greenhouseID}")
+        
+        # If the greenhouse has a channel, get its write api key
+        if "thingspeakChannel" in greenhouse.keys():
+            thingspeakInfo = greenhouse["thingspeakChannel"]
+            channel_write_api_key = thingspeakInfo["channelWriteAPIkey"]
+        
+        # Else, creayte a new channel and get its write api key
+        else:
+            channel_write_api_key = self.createGreenhouseChannel(greenhouse)
+        return channel_write_api_key
+        
+    def createGreenhouseChannel(self, greenhouse):
+        """
+        Create a new channel for the greenhouse and return its write API key.
+
+        Parameters:
+            greenhouse (dict): dictionnary containing the information on the greenhouse.
+
+        Returns:
+            str: the write API key of the created channel.
         """
         # Define the URL
-        urlToSend = f"{self.baseURL}{self.channelWriteAPIkey}&field{field_number}={field_value}"
+        urlToSend = f"{self.baseURL}channels.json"
+        name = f"Greenhouse {greenhouse["greenhouseID"]}"
+        params = {
+            "api_key": self.userAPIKey,
+            "name": name,
+            "field1": "Temperature",
+            "field2": "Moisture Zone 1",
+            "field3": "Moisture Zone 2",
+            "field4": "Moisture Zone 3",
+            "field5": "Moisture Zone 4",
+            "field6": "Moisture Zone 5",
+            "field7": "Moisture Zone 6",
+            "field8": "Moisture Zone 7"
+        }
+
+        # Create the channel
+        response = requests.post(urlToSend, data=json.dumps(params))
+
+        if response.status_code == 200:
+            channel_info = response.json()
+
+            # Get the channel information from the response
+            channel_id = channel_info["id"]
+            channel_read_api_key = ""
+            channel_write_api_key = ""
+            for api_key in channel_info["api_keys"]:
+                if api_key["write_flag"] == True:
+                    channel_write_api_key = api_key["api_key"]
+                else:
+                    channel_read_api_key = api_key["api_key"]
+
+            # Update the device in the catalog with the channel information
+            thingspeakInfo = {
+                "channelID": channel_id,
+                "channelWriteAPIkey": channel_write_api_key,
+                "channelReadAPIkey": channel_read_api_key
+            }
+            greenhouse["thingspeakInfo"] = thingspeakInfo
+            requests.put(f"{self.catalogURL}/updateGreenhouse", data=json.dumps(greenhouse))
+            
+            print(f"New channel created: {name}")
+            return channel_write_api_key
+
+        else:
+            print(f"Failed to create channel {name}: {response.text}")
         
-        # While loop to make sure that it has been updated
+
+    def uploadThingspeak(self, channel_write_api_key, field_number, field_value):
+        """
+        Upload a value in a field on Thingspeak.
+        
+        Parameters:
+            channel_write_api_key (str): write API key of the channel.
+            field_number (int): number of the field.
+            field_value (float): value to upload.
+        """
+        # Define the URL
+        urlToSend = f"{self.baseURL}update?api_key={channel_write_api_key}&field{field_number}={field_value}"
+        
+        # Ensure the update goes through
         status = 0
         while status == 0:
-            r = requests.get(urlToSend)
-            status = r.text
-            print(status) # prints if it has been updated or not -> you need to make sure it has been updated 
+            response = requests.get(urlToSend)
+            status = response.text
+            print(f"Upload status: {status}") # Debugging: Check if it was updated successfully
 
 if __name__ == "__main__":
     # Create an instance of Thingspeak_Adaptor
