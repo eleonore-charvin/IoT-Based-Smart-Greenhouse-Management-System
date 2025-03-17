8e9492a7ab0c0a4a3ce7451f0ec47a562e0f76a1
Maarco02 <s344060@studenti.polito.it>
2025-03-17 18:33:33 +0100

    Update irrigation_actuator.py

--- irrigation_actuator/irrigation_actuator.py
+++ irrigation_actuator/irrigation_actuator.py
@@ -8,7 +8,8 @@ class IrrigationActuator:
         self.client.start()
         self.client.mySubscribe(sub_topic)
         self.irrigation_status = "OFF"
-
+        self.greenhouse_id = 1
+        self.pot_id = "potID" #id of the zone 
     def notify(self, topic, payload):
         try:
             #receives the message with the action to do
@@ -27,6 +28,18 @@ class IrrigationActuator:
         except (ValueError, KeyError) as e:
             print(f"Error processing message: {e}")
 
+def start(self):
+        """Avvia l'ascolto sul topic /actuator."""
+        actuator_topic = f"{self.sub_topic}Greenhouse{self.greenhouse_id}/{self.pot_id}"
+        self.client.mySubscribe(sub_topic)
+        self.client.start()
+        print("Actuator control system started.")
+
+    def stop(self):
+        """Ferma il client MQTT."""
+        self.client.stop()
+        print("Actuator control system stopped.")
+
 if __name__ == "__main__":
     with open("settings.json", "r") as f:
         config = json.load(f)
@@ -35,10 +48,11 @@ if __name__ == "__main__":
         broker=config["broker"],
         port=config["port"], 
         irrigation_topic= config["irrigation_topic"],
+        potID = config["potID"]
     )
     try:
         while True:
-            time.sleep(10)
+            pass
     except KeyboardInterrupt:
         print("Chiusura Irrigation Actuator...")
         actuator.client.stop()
