5009e763c2f8e0538534aed261dac8d2a4b0d24a
Maarco02 <s344060@studenti.polito.it>
2025-03-28 11:12:20 +0100

    Update irrigation_actuator.py

--- irrigation_actuator/irrigation_actuator.py
+++ irrigation_actuator/irrigation_actuator.py
@@ -1,34 +1,9 @@
 import json
+
 import time
 import MyMQTT
 
 class IrrigationActuator:
-<<<<<<< HEAD
-    def __init__(self, clientID, broker, port, sub_topic):
-        self.client = MyMQTT(clientID, broker, port, self)
-        self.client.start()
-        self.client.mySubscribe(sub_topic)
-        self.irrigation_status = "OFF"
-
-    def notify(self, topic, payload):
-        try:
-            #receives the message with the action to do
-            message = json.loads(payload.decode())
-            action = message.get("action")
-            
-            #actuation of the action in the message
-            if action == "start_irrigation":
-                print("Irrigation started")
-                self.irrigation_status = "ON"
-            elif action == "stop_irrigation":
-                print("Irrigation stopped")
-                self.irrigation_status = "OFF"
-            else:
-                print("Invalid action received")
-        except (ValueError, KeyError) as e:
-            print(f"Error processing message: {e}")
-
-=======
     def __init__(self, clientID, broker, port, irrigation_topic, potID):
         self.client = MyMQTT.MyMQTT(clientID, broker, port, self)
         self.irrigation_topic = irrigation_topic
@@ -63,24 +38,11 @@ class IrrigationActuator:
         self.client.stop()
         print("Irrigation Actuator stopped.")
 
->>>>>>> d0cf3d68c964947a43f269bbe0ad6b9207443f7a
 if __name__ == "__main__":
     with open("settings.json", "r") as f:
         config = json.load(f)
 
     actuator = IrrigationActuator(
-<<<<<<< HEAD
-        broker=config["broker"],
-        port=config["port"], 
-        irrigation_topic= config["irrigation_topic"],
-    )
-    try:
-        while True:
-            time.sleep(10)
-    except KeyboardInterrupt:
-        print("Chiusura Irrigation Actuator...")
-        actuator.client.stop()
-=======
         clientID="IrrigationActuator",
         broker=config["broker"],
         port=config["port"], 
@@ -94,4 +56,3 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print("Chiusura Irrigation Actuator...")
         actuator.stop()
->>>>>>> d0cf3d68c964947a43f269bbe0ad6b9207443f7a
