1d8641f72b63501741ac56e0ad18807d5ce9e0c3
eleonore-charvin <charvin.eleonore@gmail.com>
2025-04-30 19:36:48 +0200

    fix moisture sensor

--- moisture_sensor/moisture_sensor.py
+++ moisture_sensor/moisture_sensor.py
@@ -12,15 +12,13 @@ class MoistureSensor:
         self.catalog_url = settings['catalogURL']
         self.device_info = settings['deviceInfo'].copy()
 
-        self.device_id = str(uuid.uuid4())
+        self.device_id = f"MoistureSensor{zone_id}"
         self.device_info['deviceID'] = self.device_id
-        self.device_info['greenhouseID'] = greenhouse_id
-        self.device_info['zoneID'] = zone_id
         self.moisture_level = random.randint(30, 60)
         self.irrigation_on = False
 
-        self.mqtt_broker = settings['broker']
-        self.mqtt_port = settings['port']
+        self.mqtt_broker = settings['brokerIP']
+        self.mqtt_port = settings['brokerPort']
         self.moisture_topic = settings['moistureTopic'].format(greenhouseID=greenhouse_id, zoneID=zone_id)
         self.irrigation_topic = settings['irrigationTopic'].format(greenhouseID=greenhouse_id, zoneID=zone_id)
         self.client_id = str(uuid.uuid1())
@@ -45,37 +43,36 @@ class MoistureSensor:
         Register the device in the catalog
         """  
         try:   
-            actualTime = time.time()
+            actualTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
             self.device_info["lastUpdate"] = actualTime
             params = {"zoneID": self.zone_id}
             response = requests.post(f"{self.catalog_url}/devices", params=params, data=json.dumps(self.device_info))
             response.raise_for_status()
-        except cherrypy.HTTPError as e: # Catching HTTPError
-            print(f"Error raised by catalog while registering device: {e.status} - {e.args[0]}")
+        except requests.exceptions.HTTPError as e:
+            print(f"[Greenhouse {self.greenhouse_id} zone {self.zone_id}] Error raised by catalog while registering device: {e.response.status_code} - {e.args[0]}")
         except Exception as e:
-            print(f"Error registering device in the catalog: {e}")
+            print(f"[Greenhouse {self.greenhouse_id} zone {self.zone_id}] Error registering device in the catalog: {e}")
 
     def updateDevice(self):
         """
         Update the device registration in the catalog
         """
         try:
-            actualTime = time.time()
+            actualTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
             self.device_info["lastUpdate"] = actualTime
             response = requests.put(f"{self.catalog_url}/devices", data=json.dumps(self.device_info))
             response.raise_for_status()
-        except cherrypy.HTTPError as e: # Catching HTTPError
-            print(f"Error raised by catalog while registering device: {e.status} - {e.args[0]}")
+        except requests.exceptions.HTTPError as e:
+            print(f"[Greenhouse {self.greenhouse_id} zone {self.zone_id}] Error raised by catalog while registering device: {e.response.status_code} - {e.args[0]}")
         except Exception as e:
-            print(f"Error registering device in the catalog: {e}")
+            print(f"[Greenhouse {self.greenhouse_id} zone {self.zone_id}] Error registering device in the catalog: {e}")
 
     def notify(self, topic, payload):
         try:
             message = json.loads(payload)
-            if message.get("zone_id") == self.zone_id:
-                self.irrigation_on = (message.get("command") == "ON")
+            self.irrigation_on = (message.get("command") == "ON")
         except Exception as e:
-            print(f"Error in message processing: {e}")
+            print(f"[Greenhouse {self.greenhouse_id} zone {self.zone_id}] Error in message processing: {e}")
             
     #update the moisture level with respect to the irrigation status
     def update_moisture(self):
@@ -97,8 +94,8 @@ class MoistureSensor:
         message = self._message.copy()
         message["v"] = moisture
         message["t"] = time.time()
-        self.client.myPublish(self.moisture_topic, json.dumps(message)) # publish the temperature
-        print(f"[{self.device_id} - Greenhouse {self.greenhouse_id} zone {self.zone_id}] Published moisture: {moisture} %") # print the complete message
+        self.client.myPublish(self.moisture_topic, message) # publish the moisture
+        print(f"[Greenhouse {self.greenhouse_id} zone {self.zone_id}] Published moisture {moisture} %") # print the complete message
 
 if __name__ == '__main__':
     with open('settings.json') as f:
@@ -106,7 +103,7 @@ if __name__ == '__main__':
 
     try:
         #make a list of greenhouses from the catalog
-        greenhouses = requests.get(f"{settings['catalog_url']}/greenhouses").json().get('greenhousesList', [])
+        greenhouses = requests.get(f"{settings['catalogURL']}/greenhouses").json().get('greenhousesList', [])
     except Exception as e:
         print(f"Error in retreiving greenhouses: {e}")
         greenhouses = []
